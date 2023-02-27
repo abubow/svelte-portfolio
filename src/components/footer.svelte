a92297bbd449a8806b7dfbed18fbfea78a804e52
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-02-27 21:12:52 +0500

    Added footer component

---
+++ src/components/footer.svelte
@@ -0,0 +1,27 @@
+<script>
+	import TwitterIcon from './icons/twitter-icon.svelte';
+	import YouTubeIcon from './icons/youtube-icon.svelte';
+</script>
+
+<footer class="p-10 footer bg-primary text-primary-content footer-center">
+	<div class="grid grid-flow-col gap-4">
+		<a class="link link-hover" href="/projects">Portfolio</a>
+		<a class="link link-hover" href="/posts">Blog</a>
+		<a class="link link-hover" href="/about">About</a>
+	</div>
+	<div>
+		<div class="grid grid-flow-col gap-4">
+			<a href="https://www.youtube.com/@abuzarsherazi1054" target="_blank" rel="noreferrer">
+                <YouTubeIcon />
+			</a>
+			<a href="https://twitter.com/SyedMoh25065076" target="_blank" rel="noreferrer">
+                <TwitterIcon />
+			</a>
+		</div>
+	</div>
+	<div>
+		<p>
+			Copyright &copy; {`${new Date().getFullYear()}`} - All right reserved by Abuzar Sherazi
+		</p>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.postcss';
+	import Footer from '../components/footer.svelte';
 </script>
 
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </main>
+<Footer />
