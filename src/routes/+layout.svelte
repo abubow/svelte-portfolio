4c11f03f218146ba48ced3a95e04e8c020288557
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-04-27 11:14:34 +0500

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,13 @@
 	import Navbar from '../components/navbar.svelte';
 	import { onMount } from 'svelte';
 	import { themeChange } from 'theme-change';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 	onMount(async () => {
 		themeChange(false);
 	});
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Navbar />
